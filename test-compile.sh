21b4a54983c953dc09aed4926149d7c03c37e5a7
Jan Weinschenker <jan.weinschenker@holisticon.de>
2019-01-11 14:27:53 +0100

    fix: remove unused maven profile

--- test-compile.sh
+++ test-compile.sh
@@ -3,6 +3,6 @@ source .env
 source bin/check-java-version.sh
 JAVA_VERSION_COMMAND="$JAVA_HOME/bin/java -version 2>&1 | head -n 1 | awk -F '\"' '{print $2}'"
 eval $JAVA_VERSION_COMMAND
-mvn clean jacoco:prepare-agent package jacoco:report \
+mvn clean jacoco:prepare-agent package jacoco:report -Pcodacycoverage\
   -D exec.mainClass="apodrating.MainKt" \
   -D exec.args="run apodrating.ApodRatingVerticleKt"
