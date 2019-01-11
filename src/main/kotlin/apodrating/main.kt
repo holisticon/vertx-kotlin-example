27ff62224abd5f4454fe1d1ffe9f6d883f0e273a
Jan Weinschenker <jan.weinschenker@holisticon.de>
2019-01-11 13:16:45 +0100

    chore: add codacy integration

--- src/main/kotlin/apodrating/main.kt
+++ src/main/kotlin/apodrating/main.kt
@@ -1,5 +1,6 @@
 package apodrating
 
+import io.vertx.core.DeploymentOptions
 import io.vertx.kotlin.config.ConfigRetrieverOptions
 import io.vertx.kotlin.config.ConfigStoreOptions
 import io.vertx.reactivex.core.Vertx.vertx
@@ -8,13 +9,14 @@ import mu.KotlinLogging
 private val logger = KotlinLogging.logger {}
 
 fun main(): Unit =
-    vertx().rxDeployVerticle(
-        ApodRatingVerticle::class.java.canonicalName,
-        ConfigRetrieverOptions(
-            scanPeriod = 2000,
-            stores = listOf(ConfigStoreOptions(type = "env"))
-        ).deploymentOptions(vertx())
-    )
+    vertx().rxDeployVerticle(ApodRatingVerticle::class.java.canonicalName, deploymentOptions())
         .subscribe({ logger.info { "succeeded: $it" } }) { logger.error { it } }
         .dispose()
+
+private fun deploymentOptions(): DeploymentOptions {
+    return ConfigRetrieverOptions(
+        scanPeriod = 2000,
+        stores = listOf(ConfigStoreOptions(type = "env"))
+    ).deploymentOptions(vertx())
+}
 
