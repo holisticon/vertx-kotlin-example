a11b54cac11674722f2c3c1afca72c2caf9c9860
Jan Weinschenker <jan.weinschenker@holisticon.de>
2019-01-14 12:54:38 +0100

    featz: change release plugin

--- bin/release-start.sh
+++ bin/release-start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-mvn jgitflow:release-start -DallowSnapshots=true -P no-docker -Dmaven.javadoc.skip=true
+mvn gitflow:release-start -DallowSnapshots=true -P no-docker -Dmaven.javadoc.skip=true
