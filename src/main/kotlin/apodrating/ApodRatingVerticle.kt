d7f643174ad68a3d012b7592445d06f8e8be62aa
Jan Weinschenker <jan.weinschenker@holisticon.de>
2019-09-18 00:05:37 +0200

    refactor: use rxJava / rxKotlin during startup

--- src/main/kotlin/apodrating/ApodRatingVerticle.kt
+++ src/main/kotlin/apodrating/ApodRatingVerticle.kt
@@ -9,23 +9,23 @@ import apodrating.webapi.ApodQueryService
 import apodrating.webapi.ApodQueryServiceImpl
 import apodrating.webapi.RatingService
 import apodrating.webapi.RatingServiceImpl
+import io.reactivex.Single
+import io.reactivex.functions.BiFunction
+import io.reactivex.rxkotlin.toCompletable
+import io.reactivex.schedulers.Schedulers
 import io.vertx.core.Handler
 import io.vertx.core.Promise
 import io.vertx.core.http.HttpServerOptions
-import io.vertx.ext.jdbc.JDBCClient
 import io.vertx.kotlin.core.net.pemKeyCertOptionsOf
 import io.vertx.kotlin.coroutines.CoroutineVerticle
-import io.vertx.kotlin.ext.sql.executeAwait
-import io.vertx.kotlin.ext.sql.getConnectionAwait
 import io.vertx.reactivex.core.Vertx
+import io.vertx.reactivex.core.http.HttpServer
 import io.vertx.reactivex.core.http.HttpServerRequest
+import io.vertx.reactivex.ext.jdbc.JDBCClient
 import io.vertx.reactivex.ext.web.RoutingContext
 import io.vertx.reactivex.ext.web.api.contract.openapi3.OpenAPI3RouterFactory
 import io.vertx.reactivex.ext.web.handler.StaticHandler
 import io.vertx.serviceproxy.ServiceBinder
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import mu.KLogging
 import org.apache.http.HttpStatus
@@ -48,70 +48,65 @@ class ApodRatingVerticle : CoroutineVerticle() {
      * - Initialize webserver
      */
     override fun start(startFuture: Promise<Void>?) {
-        launch {
-            val apodConfig = ApodRatingConfiguration(config)
-            val jdbcRoutine = async {
-                client = JDBCClient.createShared(vertx, apodConfig.toJdbcConfig())
-            }
-            apiKey = apodConfig.nasaApiKey
-            rxVertx = Vertx(vertx)
+        val apodConfig = ApodRatingConfiguration(config)
+        rxVertx = Vertx(vertx)
+        client = JDBCClient.createShared(rxVertx, apodConfig.toJdbcConfig())
+        apiKey = apodConfig.nasaApiKey
 
-            val serviceRoutine = async {
-                with(ServiceBinder(vertx)) {
-                    this.setAddress(RATING_SERVICE_ADDRESS).register(
-                        RatingService::class.java,
-                        RatingServiceImpl(rxVertx, config)
-                    )
-                    this.setAddress(REMOTE_PROXY_SERVICE_ADDRESS)
-                        .register(RemoteProxyService::class.java, RemoteProxyServiceImpl(rxVertx, config))
-                    this.setAddress(APOD_QUERY_ADDRESS).register(
-                        ApodQueryService::class.java,
-                        ApodQueryServiceImpl(rxVertx, config)
-                    )
-                }
-            }
-
-            val dbRoutine = async(Dispatchers.IO) {
-                jdbcRoutine.await()
-                client.getConnectionAwait().use { connection ->
-                    STATEMENTS.forEach {
-                        connection.executeAwait(it)
-                    }
-                }
-            }
-
-            val http11ServerRoutine = async(
-                Dispatchers.IO, block = startHttpServer(
-                    apodConfig.port, startFuture
+        val serviceBinderCpl = {
+            with(ServiceBinder(vertx)) {
+                this.setAddress(RATING_SERVICE_ADDRESS).register(
+                    RatingService::class.java,
+                    RatingServiceImpl(rxVertx, config)
                 )
-            )
-            val http2ServerRoutine = async(
-                Dispatchers.IO,
-                block = startHttpServer(
-                    apodConfig.h2Port,
-                    startFuture,
-                    HttpServerOptions()
-                        .setKeyCertOptions(
-                            pemKeyCertOptionsOf(certPath = "tls/server-cert.pem", keyPath = "tls/server-key.pem")
-                        )
-                        .setSsl(true)
-                        .setUseAlpn(true)
+                this.setAddress(REMOTE_PROXY_SERVICE_ADDRESS)
+                    .register(RemoteProxyService::class.java, RemoteProxyServiceImpl(rxVertx, config))
+                this.setAddress(APOD_QUERY_ADDRESS).register(
+                    ApodQueryService::class.java,
+                    ApodQueryServiceImpl(rxVertx, config)
                 )
-            )
+            }
+        }.toCompletable()
+            .subscribe()
 
-            serviceRoutine.await()
-            dbRoutine.await()
-            http11ServerRoutine.await()
-            http2ServerRoutine.await()
-            startFuture?.complete()
-        }
+        databaseObs()
+            .subscribe()
+        Single.zip(
+            http11Server(apodConfig),
+            http2Server(apodConfig),
+            BiFunction<HttpServer, HttpServer, Pair<Int, Int>> { s1, s2 ->
+                Pair(s1.actualPort(), s2.actualPort())
+            })
+            .subscribeOn(Schedulers.io())
+            .subscribe({ logger.info { "server listens on ports ${it.first} and ${it.second}" } }) {
+                startFuture?.fail("could not start http2 server.")
+            }
+
+        startFuture?.complete()
     }
+
+    private fun http2Server(apodConfig: ApodRatingConfiguration): Single<HttpServer> = startHttpServer(
+        apodConfig.h2Port,
+        HttpServerOptions()
+            .setKeyCertOptions(
+                pemKeyCertOptionsOf(certPath = "tls/server-cert.pem", keyPath = "tls/server-key.pem")
+            )
+            .setSsl(true)
+            .setUseAlpn(true)
+    )
+
+    private fun http11Server(apodConfig: ApodRatingConfiguration): Single<HttpServer> = startHttpServer(apodConfig.port)
+
+    private fun databaseObs(): Single<Int> = client
+        .rxGetConnection()
+        .flatMap { it.rxBatch(STATEMENTS) }
+        .map { it.size }
+        .subscribeOn(Schedulers.computation())
 
     private fun startHttpServer(
         port: Int,
-        startFuture: Promise<Void>?,
         httpServerOptions: HttpServerOptions? = null
-    ): suspend CoroutineScope.() -> Unit = {
+    ): Single<HttpServer> =
         OpenAPI3RouterFactory.rxCreate(rxVertx, "swagger.yaml")
             .map { it.mountServicesFromExtensions() }
             .map { routerFactory ->
@@ -119,10 +114,6 @@ class ApodRatingVerticle : CoroutineVerticle() {
                     ?: rxVertx.createHttpServer()).requestHandler(createRouter(routerFactory))
             }
             .flatMap { it.rxListen(port) }
-            .subscribe({ logger.info { "server listens on port ${it.actualPort()}" } }) {
-                startFuture?.fail("could not start http2 server.")
-            }
-    }
 
     private fun createRouter(routerFactory: OpenAPI3RouterFactory): Handler<HttpServerRequest> =
         routerFactory.apply {
