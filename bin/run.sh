53183cf5fb673ad20595d236850d3c780f5d5870
Jan Weinschenker <jan.weinschenker@holisticon.de>
2019-12-04 12:57:12 +0100

    feat: change to async log4j2 logger

--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,6 @@ JAVA_VERSION_COMMAND="$JAVA_HOME/bin/java -version 2>&1 | head -n 1 | awk -F '\"
 eval $JAVA_VERSION_COMMAND
 
 $JAVA_HOME/bin/java $J_ARG_LINE -jar ../target/application.jar \
-  -D exec.mainClass="apodrating.MainKt"
+  -D exec.mainClass="apodrating.MainKt" \
+  -D vertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
+  -D log4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
--- bin/test-compile.sh
+++ bin/test-compile.sh
@@ -5,5 +5,7 @@ JAVA_VERSION_COMMAND="$JAVA_HOME/bin/java -version 2>&1 | head -n 1 | awk -F '\"
 eval $JAVA_VERSION_COMMAND
 mvn clean jacoco:prepare-agent package jacoco:report \
   -D exec.mainClass="apodrating.MainKt" \
-  -Dmdep.outputFile=classpath.txt \
+  -D mdep.outputFile=classpath.txt \
+  -D vertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
+  -D log4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
   -f ../pom.xml
