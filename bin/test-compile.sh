6b9892a40c355084ad7b53244e83848a7d8e8482
Jan Weinschenker <jan.weinschenker@holisticon.de>
2019-12-05 22:31:23 +0100

    feat: use multi threaded build

--- bin/test-compile.sh
+++ bin/test-compile.sh
@@ -3,7 +3,7 @@ source ../.env
 source ./check-java-version.sh
 JAVA_VERSION_COMMAND="$JAVA_HOME/bin/java -version 2>&1 | head -n 1 | awk -F '\"' '{print $2}'"
 eval $JAVA_VERSION_COMMAND
-mvn clean jacoco:prepare-agent package jacoco:report \
+mvn clean -T 2C jacoco:prepare-agent package jacoco:report \
   -Dexec.mainClass="apodrating.MainKt" \
   -Dmdep.outputFile=classpath.txt \
   -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
