b58fbb090614f4818056957c235092c6a2d9eed8
Jan Weinschenker <jan.weinschenker@holisticon.de>
2020-02-13 09:26:48 +0100

    chore: version updates

--- bin/test-compile.sh
+++ bin/test-compile.sh
@@ -3,9 +3,10 @@ source ../.env
 source ./check-java-version.sh
 JAVA_VERSION_COMMAND="$JAVA_HOME/bin/java -version 2>&1 | head -n 1 | awk -F '\"' '{print $2}'"
 eval $JAVA_VERSION_COMMAND
-mvn clean -T 2C jacoco:prepare-agent package jacoco:report \
+mvn clean test \
   -Dexec.mainClass="apodrating.MainKt" \
   -Dmdep.outputFile=classpath.txt \
+  -DargLine="" \
   -Dvertx.logger-delegate-factory-class-name=io.vertx.core.logging.Log4j2LogDelegateFactory \
   -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector \
-  -f ../pom.xml
+  -f ../pom.xml -U
